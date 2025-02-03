f226a0b4e9b93969eda06f53b0c8f9d14463f75d
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-02-03 22:42:15 +0530

    Time: 0 ms (100.00%) | Memory: 52.7 MB (15.40%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.js
+++ 412-fizz-buzz/fizz-buzz.js
@@ -20,13 +20,13 @@ var fizzBuzz = function (n) {
 //     let answer = [];
 //     for (let i = 1; i <= n; i++) {
 //         if (i % 3 === 0 && i % 5 === 0) {
-//             answer.push("FizzBuzz"); // Divisible by both 3 and 5
+//             answer.push("FizzBuzz");
 //         } else if (i % 3 === 0) {
-//             answer.push("Fizz"); // Divisible by 3
+//             answer.push("Fizz");
 //         } else if (i % 5 === 0) {
-//             answer.push("Buzz"); // Divisible by 5
+//             answer.push("Buzz");
 //         } else {
-//             answer.push(i.toString()); // Not divisible by 3 or 5
+//             answer.push(i.toString());
 //         }
 //     }
 //     return answer;

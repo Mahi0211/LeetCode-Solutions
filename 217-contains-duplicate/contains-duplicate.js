a98cd13e6d8cc9a130688f8c3c2df42635923092
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-02-06 00:23:47 +0530

    Time: 12 ms (70.64%) | Memory: 63 MB (25.99%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -13,3 +13,14 @@ var containsDuplicate = function (nums) {
     }
     return false;
 };
+
+// var containsDuplicate = function(nums) {
+//     let seen = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         if (seen.includes(nums[i])) {
+//             return true;
+//         }
+//         seen.push(nums[i]);
+//     }
+//     return false;
+// }; //this is worst practice - time limit exceeded

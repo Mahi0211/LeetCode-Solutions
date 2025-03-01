d78a781decbfa7e78f91954963582ff2795974f6
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-01 21:15:57 +0530

    Time: 50 ms (70.08%) | Memory: 53 MB (15.69%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -3,30 +3,44 @@
  * @param {string} word2
  * @return {string}
  */
+
 var mergeAlternately = function (word1, word2) {
     let A = word1.length, B = word2.length
     let a = 0, b = 0
-    let word = 1
-    let str = ""
+    let result = []
     while (a < A && b < B) {
-        if (word === 1) {
-            str += word1.charAt(a);
-            a++
-            word = 2
-        } else {
-            str += word2.charAt(b);
-            b++
-            word = 1
-        }
+        result.push(word1.charAt(a++))
+        result.push(word2.charAt(b++))
     }
+    while (a < A) result.push(word1.charAt(a++))
+    while (b < B) result.push(word2.charAt(b++))
+    return result.join("")
+}
 
-    while (a < A) {
-        str += word1.charAt(a);
-        a++
-    }
-    while (b < B) {
-        str += word2.charAt(b);
-        b++
-    }
-    return str
-};
+// var mergeAlternately = function (word1, word2) {
+//     let A = word1.length, B = word2.length
+//     let a = 0, b = 0
+//     let word = 1
+//     let str = ""
+//     while (a < A && b < B) {
+//         if (word === 1) {
+//             str += word1.charAt(a);
+//             a++
+//             word = 2
+//         } else {
+//             str += word2.charAt(b);
+//             b++
+//             word = 1
+//         }
+//     }
+
+//     while (a < A) {
+//         str += word1.charAt(a);
+//         a++
+//     }
+//     while (b < B) {
+//         str += word2.charAt(b);
+//         b++
+//     }
+//     return str
+// };

49e3a96e88e01018e507b8a920739ea3f4cabe36
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-01 21:22:05 +0530

    Time: 44 ms (89.02%) | Memory: 53.8 MB (10.98%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,43 +4,35 @@
  * @return {string}
  */
 
-var mergeAlternately = function (word1, word2) {
-    let A = word1.length, B = word2.length
-    let a = 0, b = 0
-    let result = []
-    while (a < A && b < B) {
-        result.push(word1.charAt(a++))
-        result.push(word2.charAt(b++))
-    }
-    while (a < A) result.push(word1.charAt(a++))
-    while (b < B) result.push(word2.charAt(b++))
-    return result.join("")
-}
-
 // var mergeAlternately = function (word1, word2) {
 //     let A = word1.length, B = word2.length
 //     let a = 0, b = 0
-//     let word = 1
-//     let str = ""
+//     let result = []
 //     while (a < A && b < B) {
-//         if (word === 1) {
-//             str += word1.charAt(a);
-//             a++
-//             word = 2
-//         } else {
-//             str += word2.charAt(b);
-//             b++
-//             word = 1
-//         }
+//         result.push(word1.charAt(a++))
+//         result.push(word2.charAt(b++))
 //     }
+//     while (a < A) result.push(word1.charAt(a++))
+//     while (b < B) result.push(word2.charAt(b++))
+//     return result.join("")
+// }
 
-//     while (a < A) {
-//         str += word1.charAt(a);
-//         a++
-//     }
-//     while (b < B) {
-//         str += word2.charAt(b);
-//         b++
-//     }
-//     return str
-// };
+var mergeAlternately = function (word1, word2) {
+    let A = word1.length, B = word2.length
+    let a = 0, b = 0
+    let word = 1
+    let str = ""
+    while (a < A && b < B) {
+        if (word === 1) {
+            str += word1.charAt(a++);
+            word = 2
+        } else {
+            str += word2.charAt(b++);
+            word = 1
+        }
+    }
+
+    while (a < A) str += word1.charAt(a++)
+    while (b < B) str += word2.charAt(b++)
+    return str
+};

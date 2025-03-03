f5a6524a84dca596b604994556e3df7be3fc19fe
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-03 22:15:29 +0530

    Time: 3 ms (56.31%) | Memory: 65.2 MB (11.62%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,27 +2,27 @@
  * @param {number[]} prices
  * @return {number}
  */
-// var maxProfit = function (prices) {
-//     let i = 0
-//     let max = 0
-//     for (let j = 1; j < prices.length; j++) {
-//         if (prices[i] < prices[j]) {
-//             max = Math.max(max, prices[j] - prices[i])
-//         } else {
-//             i = j
-//         }
-//     }
-//     return max
-// };
-
 var maxProfit = function (prices) {
-    let minPrice = Infinity;
-    let maxProfit = 0;
-    
-    for (let price of prices) {
-        minPrice = Math.min(minPrice, price);
-        maxProfit = Math.max(maxProfit, price - minPrice);
+    let i = 0
+    let max = 0
+    for (let j = 1; j < prices.length; j++) {
+        if (prices[i] < prices[j]) {
+            max = Math.max(max, prices[j] - prices[i])
+        } else {
+            i = j
+        }
     }
-    
-    return maxProfit;
+    return max
 };
+
+// var maxProfit = function (prices) {
+//     let minPrice = Infinity;
+//     let maxProfit = 0;
+    
+//     for (let price of prices) {
+//         minPrice = Math.min(minPrice, price);
+//         maxProfit = Math.max(maxProfit, price - minPrice);
+//     }
+    
+//     return maxProfit;
+// };

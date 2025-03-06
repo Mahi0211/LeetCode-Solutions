20071dd7cb577891e80e2749110b0d5431c6d47f
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-07 02:15:30 +0530

    Time: 5 ms (87.43%) | Memory: 63.5 MB (23.26%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -2,6 +2,27 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    return x<0 ? false : x === +x.toString().split("").reverse().join("");
-};
+
+var isPalindrome = function (x) {
+    if (x < 0 || (x % 10 === 0 && x !== 0)) return false
+
+    let reversed = 0;
+    let original = x;
+
+    while (x > 0) {
+        reversed = reversed * 10 + (x % 10);
+        x = Math.floor(x / 10)
+    }
+    return reversed === original
+}
+
+// var isPalindrome = function (x) {
+//     if (x < 0) return false;
+//     if (x < 10) return true;
+
+//     let str = x.toString();
+//     if (str[0] !== str[str.length - 1]) {
+//         return false;
+//     }
+//     return isPalindrome(parseInt(str.slice(1, -1)) || 0);
+// };

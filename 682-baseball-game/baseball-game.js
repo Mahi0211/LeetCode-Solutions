0b031f8eadfc190c35eeda2d14da9dc66f8c6d05
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-10 15:45:28 +0530

    Time: 1 ms (75.36%) | Memory: 53.8 MB (30.45%) - LeetSync

--- 682-baseball-game/baseball-game.js
+++ 682-baseball-game/baseball-game.js
@@ -2,22 +2,45 @@
  * @param {string[]} operations
  * @return {number}
  */
+
 var calPoints = function (operations) {
     let stack = [];
 
     for (let val of operations) {
-        if (!isNaN(val)) {
-            stack.push(parseInt(val));
-        } else {
-            if (val === "C") {
+        switch (val) {
+            case 'C':
                 stack.pop();
-            } else if (val === "D") {
+                break;
+            case 'D':
                 stack.push(stack[stack.length - 1] * 2);
-            } else if (val === "+") {
+                break;
+            case '+':
                 stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
-            }
+                break;
+            default:
+                stack.push(parseInt(val));
         }
     }
     let result = stack.reduce((a, b) => a + b, 0);
     return result;
-};
+}
+
+// var calPoints = function (operations) {
+//     let stack = [];
+
+//     for (let val of operations) {
+//         if (!isNaN(val)) {
+//             stack.push(parseInt(val));
+//         } else {
+//             if (val === "C") {
+//                 stack.pop();
+//             } else if (val === "D") {
+//                 stack.push(stack[stack.length - 1] * 2);
+//             } else if (val === "+") {
+//                 stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+//             }
+//         }
+//     }
+//     let result = stack.reduce((a, b) => a + b, 0);
+//     return result;
+// };

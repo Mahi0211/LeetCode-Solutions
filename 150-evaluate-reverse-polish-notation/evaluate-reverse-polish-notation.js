3c71daa314c7c1590c17c07f949baeba6e383fa5
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-10 18:46:41 +0530

    Time: 6 ms (60.62%) | Memory: 57.5 MB (30.43%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -2,30 +2,50 @@
  * @param {string[]} tokens
  * @return {number}
  */
-var evalRPN = function (tokens) {
-  let stack = [];
 
-  for (let val of tokens) {
-    switch (val) {
-      case "+":
-        stack.push(stack.pop() + stack.pop());
-        break;
-      case "-":
-        let b = stack.pop();
-        let a = stack.pop();
-        stack.push(a - b);
-        break;
-      case "*":
-        stack.push(stack.pop() * stack.pop());
-        break;
-      case "/":
-        let divisor = stack.pop();
-        let dividend = stack.pop();
-        stack.push(Math.trunc(dividend / divisor));
-        break;
-      default:
-        stack.push(parseInt(val));
+var evalRPN = function (tokens) {
+    let stack = [];
+    let operators = {
+        "+": (b, a) => a + b,
+        "-": (b, a) => a - b,
+        "*": (b, a) => a * b,
+        "/": (b, a) => Math.trunc(a / b),
+    };
+
+    for (let val of tokens) {
+        if (operators[val]) {
+            stack.push(operators[val](stack.pop(), stack.pop()));
+        } else {
+            stack.push(parseInt(val));
+        }
     }
-  }
-  return stack[0];
+    return stack[0];
 };
+
+// var evalRPN = function (tokens) {
+//   let stack = [];
+
+//   for (let val of tokens) {
+//     switch (val) {
+//       case "+":
+//         stack.push(stack.pop() + stack.pop());
+//         break;
+//       case "-":
+//         let b = stack.pop();
+//         let a = stack.pop();
+//         stack.push(a - b);
+//         break;
+//       case "*":
+//         stack.push(stack.pop() * stack.pop());
+//         break;
+//       case "/":
+//         let divisor = stack.pop();
+//         let dividend = stack.pop();
+//         stack.push(Math.trunc(dividend / divisor));
+//         break;
+//       default:
+//         stack.push(parseInt(val));
+//     }
+//   }
+//   return stack[0];
+// };

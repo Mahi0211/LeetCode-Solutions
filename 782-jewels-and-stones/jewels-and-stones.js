168dd56a61beccb66bebd7de82a8ec1e1918b795
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-10 20:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 54.3 MB (11.05%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -3,18 +3,31 @@
  * @param {string} stones
  * @return {number}
  */
+
 var numJewelsInStones = function (jewels, stones) {
-    let stoneSet = new Map();
+    let jewelSet = new Set(jewels)
     let result = 0;
 
     for (let stone of stones) {
-        stoneSet.set(stone, (stoneSet.get(stone) || 0) + 1);
-    }
-
-    for (let jewel of jewels) {
-        if (stoneSet.has(jewel)) {
-            result += stoneSet.get(jewel);
+        if (jewelSet.has(stone)) {
+            result++
         }
     }
     return result;
 };
+
+// var numJewelsInStones = function (jewels, stones) {
+//     let stoneSet = new Map();
+//     let result = 0;
+
+//     for (let stone of stones) {
+//         stoneSet.set(stone, (stoneSet.get(stone) || 0) + 1);
+//     }
+
+//     for (let jewel of jewels) {
+//         if (stoneSet.has(jewel)) {
+//             result += stoneSet.get(jewel);
+//         }
+//     }
+//     return result;
+// };

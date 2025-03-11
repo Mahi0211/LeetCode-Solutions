f804193901d44958a91f22a572f171f057a4af34
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-11 16:30:05 +0530

    Time: 22 ms (61.90%) | Memory: 81.1 MB (8.84%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -2,16 +2,32 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function (temperatures) {
-    let stack = [];
-    let ans = new Array(temperatures.length).fill(0)
 
-    for (let val in temperatures) {
-        while (stack.length > 0 && temperatures[val] > temperatures[stack[stack.length - 1]]) {
-            let prevIndex = stack.pop();
-            ans[prevIndex] = val - prevIndex;
+var dailyTemperatures = function (temperatures) {
+    const n = temperatures.length;
+    const answer = new Array(n).fill(0);
+    const stack = [];
+
+    for (let i = 0; i < n; i++) {
+        while (stack.length && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const prevIndex = stack.pop();
+            answer[prevIndex] = i - prevIndex;
         }
-        stack.push(val)
+        stack.push(i);
     }
-    return ans;
+    return answer;
 };
+
+// var dailyTemperatures = function (temperatures) {
+//     let stack = [];
+//     let ans = new Array(temperatures.length).fill(0)
+
+//     for (let val in temperatures) {
+//         while (stack.length > 0 && temperatures[val] > temperatures[stack[stack.length - 1]]) {
+//             let prevIndex = stack.pop();
+//             ans[prevIndex] = val - prevIndex;
+//         }
+//         stack.push(val)
+//     }
+//     return ans;
+// };

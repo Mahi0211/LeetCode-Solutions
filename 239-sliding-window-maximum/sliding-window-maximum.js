e91a83aca1c05c5dd01f38330eb4cbcc3e1b174d
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-13 18:38:56 +0530

    Time: 427 ms (51.78%) | Memory: 89.1 MB (10.82%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+
+var maxSlidingWindow = function (nums, k) {
+    let result = [];
+    let dequeue = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (dequeue.length > 0 && dequeue[0] <= i - k) {
+            dequeue.shift()
+        }
+
+        while (dequeue.length > 0 && nums[dequeue.at(-1)] < nums[i]) {
+            dequeue.pop()
+        }
+        dequeue.push(i)
+
+        if (i >= k - 1) {
+            result.push(nums[dequeue[0]])
+        }
+    }
+    return result;
+}
+
+// var maxSlidingWindow = function (nums, k) {
+//     const result = [];
+//     const n = nums.length;
+//     for (let i = 0; i <= n - k; i++) {
+//         let max = nums[i];
+//         for (let j = 1; j < k; j++) {
+//             if (nums[i + j] > max) {
+//                 max = nums[i + j];
+//             }
+//         }
+//         result.push(max);
+//     }
+//     return result;
+// };

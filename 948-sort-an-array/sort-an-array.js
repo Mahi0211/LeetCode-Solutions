0d9791c297afb2c879d610bd5f93cf02a807f701
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-20 00:40:29 +0530

    Time: 44 ms (91.70%) | Memory: 74.2 MB (64.04%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// var sortArray = function (nums) {
+//     const n = nums.length;
+//     for (let i = 0; i < n; i++) {
+//         let swapped = false
+//         for (let j = 0; j < n - i - 1; j++) {
+//             if (nums[j] > nums[j + 1]) {
+//                 // [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+//                 let temp = nums[j]
+//                 nums[j] = nums[j + 1]
+//                 nums[j + 1] = temp
+//                 swapped = true;
+//             }
+//         }
+//         // If no swaps were made in this pass, the array is sorted
+//         if (!swapped) break;
+//     }
+//     return nums
+// };
+
+var sortArray = function (nums) {
+    return nums.sort((a, b) => a - b);
+};

efa5a5cee3f6e2715824106b508216eebf4ffc34
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-20 01:34:25 +0530

    Time: 4 ms (9.40%) | Memory: 58 MB (5.24%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -2,19 +2,35 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+// var sortColors = function (nums) {
+//     let n = nums.length
+//     let low = 0, mid = 0, high = n - 1
+//     while (mid <= high) {
+//         if (nums[mid] === 0) {
+//             [nums[low], nums[mid]] = [nums[mid], nums[low]]
+//             low++
+//             mid++
+//         } else if (nums[mid] === 1) {
+//             mid++
+//         } else if (nums[mid] === 2) {
+//             [nums[mid], nums[high]] = [nums[high], nums[mid]]
+//             high--
+//         }
+//     }
+// };
+
 var sortColors = function (nums) {
-    let n = nums.length
-    let low = 0, mid = 0, high = n - 1
-    while (mid <= high) {
-        if (nums[mid] === 0) {
-            [nums[low], nums[mid]] = [nums[mid], nums[low]]
-            low++
-            mid++
-        } else if (nums[mid] === 1) {
-            mid++
-        } else if (nums[mid] === 2) {
-            [nums[mid], nums[high]] = [nums[high], nums[mid]]
-            high--
+    let n = nums.length - 1;
+    let swapped = true;
+    while (swapped) {
+        swapped = false;
+        for (let i = 0; i < n; i++) {
+            if (nums[i] > nums[i + 1]) {
+                [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]]
+                swapped = true
+            }
         }
+        n--
     }
-};
+    return nums
+}

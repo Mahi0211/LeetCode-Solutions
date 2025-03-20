0c4bbcb588f592ab10d41de6f1923dfeb229f986
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-20 20:10:24 +0530

    Time: 44 ms (91.77%) | Memory: 74.9 MB (52.20%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -3,23 +3,44 @@
  * @return {number[]}
  */
 
+//using Selection sort
 var sortArray = function (nums) {
-    let unsortedIndex = nums.length - 1;
-    let swapped = true;
-
-    while (swapped) {
-        swapped = false;
-
-        for (let i = 0; i < unsortedIndex; i++) {
-            if (nums[i] > nums[i + 1]) {
-                let temp = nums[i];
-                nums[i] = nums[i + 1];
-                swapped = true;
+    let n = nums.length;
+    for (let i = 0; i < n - 1; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < n; j++) {
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j;
             }
         }
-        unsortedIndex--;
+        if (minIndex !== i) {
+            let temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
     }
+    return nums;
 };
+
+// var sortArray = function (nums) {
+//     let unsortedIndex = nums.length - 1;
+//     let swapped = true;
+
+//     while (swapped) {
+//         swapped = false;
+
+//         for (let i = 0; i < unsortedIndex; i++) {
+//             if (nums[i] > nums[i + 1]) {
+//                 let temp = nums[i];
+//                 nums[i] = nums[i + 1];
+//                 nums[i+1] = temp
+//                 swapped = true;
+//             }
+//         }
+//         unsortedIndex--;
+//     }
+//     return nums
+// }; // bubble sort
 
 // var sortArray = function (nums) {
 //     const n = nums.length;
@@ -43,3 +64,5 @@ var sortArray = function (nums) {
 var sortArray = function (nums) {
     return nums.sort((a, b) => a - b);
 }; //Timesort (inbuils js)
+
+// You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.

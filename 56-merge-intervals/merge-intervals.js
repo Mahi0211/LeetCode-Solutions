e56efd9489bca330ee7549f069687e0ec482348a
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-03-25 02:35:16 +0530

    Time: 33 ms (8.34%) | Memory: 68.3 MB (5.80%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -6,7 +6,8 @@ var merge = function (intervals) {
     let startIndex = 0;
     let endIndex = 1;
 
-    intervals.sort((a, b) => a[startIndex] - b[startIndex]); //sort using inbuild sort method
+    //   intervals.sort((a, b) => a[startIndex] - b[startIndex]); //sort using inbuild sort method
+    intervals = mergeSort(intervals);
 
     const mergeIntervals = [];
 
@@ -27,3 +28,26 @@ var merge = function (intervals) {
     mergeIntervals.push([mergeStart, mergeEnd]);
     return mergeIntervals;
 };
+
+var mergeSort = function (nums) {
+    //base case
+    if (nums.length <= 1) return nums;
+
+    let mid = Math.floor(nums.length / 2);
+    let left = mergeSort(nums.slice(0, mid));
+    let right = mergeSort(nums.slice(mid));
+
+    return mergeArray(left, right);
+};
+
+function mergeArray(left, right) {
+    let sortedArr = [];
+    while (left.length && right.length) {
+        if (left[0][0] < right[0][0]) {
+            sortedArr.push(left.shift());
+        } else {
+            sortedArr.push(right.shift());
+        }
+    }
+    return [...sortedArr, ...left, ...right];
+}

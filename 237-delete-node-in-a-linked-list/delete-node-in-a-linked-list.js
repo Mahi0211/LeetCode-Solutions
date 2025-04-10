83858f99a179c5bbb562009fed83c7255f200500
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-04-11 01:31:12 +0530

    Time: 45 ms (86.45%) | Memory: 58 MB (23.53%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.js
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.js
@@ -10,9 +10,6 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function (node) {
-    //4 -> 5 -> 1 -> 9
     node.val = node.next.val;
-    //4 -> 1 -> 1 -> 9
     node.next = node.next.next;
-    //4 -> 1 -> 9
 };

b91ed23f26258cf24b757f992614bf109273a935
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-04-12 01:50:44 +0530

    Time: 2 ms (88.70%) | Memory: 61.1 MB (70.76%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.js
+++ 2-add-two-numbers/add-two-numbers.js
@@ -11,15 +11,15 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function (l1, l2) {
-    var dummy = new ListNode(0);
-    var current = dummy;
-    var carry = 0;
+    let dummy = new ListNode(0);
+    let current = dummy;
+    let carry = 0;
 
     while (l1 != null || l2 != null || carry > 0) {
-        var val1 = l1 ? l1.val : 0;
-        var val2 = l2 ? l2.val : 0;
+        let val1 = l1 ? l1.val : 0;
+        let val2 = l2 ? l2.val : 0;
 
-        var sum = val1 + val2 + carry;
+        let sum = val1 + val2 + carry;
         carry = Math.floor(sum / 10);
         sum %= 10;
 

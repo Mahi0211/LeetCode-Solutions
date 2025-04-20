8b0afaff51d23028e939c713146cb4572d3c3cc0
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-04-21 01:58:23 +0530

    Time: 5 ms (48.03%) | Memory: 59.2 MB (15.97%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -7,20 +7,20 @@ var majorityElement = function (nums) {
     let counts = new Map();
     let majorityElement = Math.floor(nums.length / 2);
 
-    // for (let num of nums) {
-    //     counts.set(num, (counts.get(num) || 0) + 1);
-    //     if (counts.get(num) > mejorityElement) {
-    //         return num;
-    //     }
-    // }
-
-    for (let i = 0; i < nums.length; i++) {
-        let num = nums[i];
+    for (let num of nums) {
         counts.set(num, (counts.get(num) || 0) + 1);
         if (counts.get(num) > majorityElement) {
             return num;
         }
     }
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     let num = nums[i];
+    //     counts.set(num, (counts.get(num) || 0) + 1);
+    //     if (counts.get(num) > majorityElement) {
+    //         return num;
+    //     }
+    // }
     return null
 }
 

b4ef83ab7d99da67b4b469b23119aa0537ce0a38
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-04-22 02:44:27 +0530

    Time: 7 ms (31.26%) | Memory: 55.9 MB (75.73%) - LeetSync

--- 38-count-and-say/count-and-say.js
+++ 38-count-and-say/count-and-say.js
@@ -2,43 +2,41 @@
  * @param {number} n
  * @return {string}
  */
+var countAndSay = function (n) {
+    if (n === 1) return "1";
+    let prev = "1";
+    for (let i = 2; i <= n; i++) {
+        let newSeq = "";
+        let count = 1
+        for (let j = 1; j < prev.length; j++) {
+            if (prev[j] === prev[j - 1]) {
+                count++;
+            } else {
+                newSeq += count.toString() + prev[j - 1]
+                count = 1
+            }
+        }
+        newSeq += count.toString() + prev[prev.length - 1];
+        prev = newSeq;
+    }
+    return prev;
+};
+
 // var countAndSay = function (n) {
 //     if (n === 1) return "1";
 
-//     let prev = "1";
+//     let prev = countAndSay(n - 1);
+//     let result = "";
+//     let count = 1
 
-//     for (let i = 2; i <= n; i++) {
-//         let newSeq = "";
-//         let count = 1
-//         for (let j = 1; j < prev.length; j++) {
-//             if (prev[j] === prev[j - 1]) {
-//                 count++;
-//             } else {
-//                 newSeq += count.toString() + prev[j - 1]
-//                 count = 1
-//             }
+//     for (let i = 1; i < prev.length; i++) {
+//         if (prev[i] === prev[i - 1]) {
+//             count++;
+//         } else {
+//             result += count + prev[i - 1];
+//             count = 1;
 //         }
-//         newSeq += count.toString() + prev[prev.length - 1];
-//         prev = newSeq;
 //     }
-//     return prev;
-// };
-
-var countAndSay = function (n) {
-    if (n === 1) return "1";
-
-    let prev = countAndSay(n - 1);
-    let result = "";
-    let count = 1
-
-    for (let i = 1; i < prev.length; i++) {
-        if (prev[i] === prev[i - 1]) {
-            count++;
-        } else {
-            result += count + prev[i - 1];
-            count = 1;
-        }
-    }
-    result += count + prev[prev.length - 1];
-    return result;
-}
+//     result += count + prev[prev.length - 1];
+//     return result;
+// }

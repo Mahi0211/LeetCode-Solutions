1641630840590ce6df5872ff909068546d109b8b
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-04-28 17:23:55 +0530

    Time: 2 ms (7.91%) | Memory: 57.4 MB (7.75%) - LeetSync

--- 78-subsets/subsets.js
+++ 78-subsets/subsets.js
@@ -2,19 +2,35 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// var subsets = function (nums) {
+//     let temp = [];
+//     let result = [];
+//     let n = nums;
+//     function recusrive(n, i) {
+//         if (i === n.length) {
+//             return result.push([...temp]);
+//         }
+//         temp.push(n[i]);
+//         recusrive(n, i + 1);
+//         temp.pop();
+//         recusrive(n, i + 1);
+//     }
+//     recusrive(n, 0);
+//     return result;
+// };
+
 var subsets = function (nums) {
-    let temp = [];
-    let result = [];
-    let n = nums;
-    function recusrive(n, i) {
-        if (i === n.length) {
-            return result.push([...temp]);
+    let ans = [];
+    let n = nums.length;
+    let subsets = 1 << n;
+    for (let i = 0; i < subsets; i++) {
+        let subset = [];
+        for (let j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                subset.push(nums[j])
+            }
         }
-        temp.push(n[i]);
-        recusrive(n, i + 1);
-        temp.pop();
-        recusrive(n, i + 1);
+        ans.push(subset)
     }
-    recusrive(n, 0);
-    return result;
-};
+    return ans;
+}

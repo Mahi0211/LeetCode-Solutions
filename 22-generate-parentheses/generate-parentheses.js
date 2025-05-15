f4eb242b5d8689bfef68137107cf980bbd7fbe83
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-05-15 22:22:39 +0530

    Time: 1 ms (88.10%) | Memory: 54.2 MB (66.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.js
+++ 22-generate-parentheses/generate-parentheses.js
@@ -2,24 +2,39 @@
  * @param {number} n
  * @return {string[]}
  */
+
 var generateParenthesis = function (n) {
-    let res = [], temp = [];
-    const backtrack = (open, close) => {
-        if (open === n && close === n) {
-            res.push(temp.join(''))
+    let res = [];
+    const backtrack = (s, open, close) => {
+        if (s.length === n * 2) {
+            res.push(s)
             return
         }
-        if (open < n) {
-            temp.push('(')
-            backtrack(open + 1, close)
-            temp.pop()
-        }
-        if (close < open) {
-            temp.push(')')
-            backtrack(open, close + 1)
-            temp.pop()
-        }
+        if (open < n) backtrack(s + "(", open + 1, close);
+        if (close < open) backtrack(s + ")", open, close + 1);
     }
-    backtrack(0, 0)
+    backtrack("", 0, 0)
     return res;
 };
+
+// var generateParenthesis = function (n) {
+//     let res = [], temp = [];
+//     const backtrack = (open, close) => {
+//         if (open === n && close === n) {
+//             res.push(temp.join(''))
+//             return
+//         }
+//         if (open < n) {
+//             temp.push('(')
+//             backtrack(open + 1, close)
+//             temp.pop()
+//         }
+//         if (close < open) {
+//             temp.push(')')
+//             backtrack(open, close + 1)
+//             temp.pop()
+//         }
+//     }
+//     backtrack(0, 0)
+//     return res;
+// };

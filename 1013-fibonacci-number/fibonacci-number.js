ffbfab53dcec863daa3fccdfd71b0097f08c9d2e
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-05-16 21:55:50 +0530

    Time: 32 ms (96.06%) | Memory: 53.5 MB (54.33%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,11 +3,24 @@
  * @return {number}
  */
 
-var fib = function (n, dp = {}) {
-    if (n <= 1) return n;
-    if (dp[n] !== undefined) return dp[n];
-    return dp[n] = fib(n - 1, dp) + fib(n - 2, dp)
-}
+var fib = function (n) {
+    let dp = new Array(n + 1).fill(0);
+    if (n > 0) dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+};
+
+//Memoization (Top-down DP with recursion)
+
+// var fib = function (n, dp = {}) {
+//     if (n <= 1) return n;
+//     if (dp[n] !== undefined) return dp[n];
+//     return dp[n] = fib(n - 1, dp) + fib(n - 2, dp)
+// }
+
+//Space-optimized DP (this approach\U0001f447!)
 
 // var fib = function (n) {
 //     if (n <= 1) return n;

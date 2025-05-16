04fa4598ec50fe19ea678d70153a6acad0c7ea05
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-05-16 20:15:42 +0530

    Time: 44 ms (61.68%) | Memory: 53.4 MB (54.33%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -3,17 +3,23 @@
  * @return {number}
  */
 
-var fib = function (n) {
+var fib = function (n, dp = {}) {
     if (n <= 1) return n;
-    let a = 0, b = 1;
-    for (let i = 2; i <= n; i++) {
-        // [a, b] = [b, a + b]
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return b;
+    if (dp[n] !== undefined) return dp[n];
+    return dp[n] = fib(n - 1, dp) + fib(n - 2, dp)
 }
+
+// var fib = function (n) {
+//     if (n <= 1) return n;
+//     let a = 0, b = 1;
+//     for (let i = 2; i <= n; i++) {
+//         // [a, b] = [b, a + b]
+//         let temp = a + b;
+//         a = b;
+//         b = temp;
+//     }
+//     return b;
+// }
 
 // var fib = function (n) {
 //     if (n <= 1) return n;

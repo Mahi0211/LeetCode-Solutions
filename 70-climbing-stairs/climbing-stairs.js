252f5ff43ec24232b579fcb967ead03eaabf0ee8
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-05-17 20:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (23.79%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -2,12 +2,30 @@
  * @param {number} n
  * @return {number}
  */
+
+//  memoization
+
+// var climbStairs = function (n, memo = {}) {
+//     if (n in memo) return memo[n];
+//     if (n <= 0) return 0;
+//     if (n === 1) return 1;
+//     if (n === 2) return 2;
+
+//     memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+//     return memo[n];
+// };
+
+//  Tabulation
+
 var climbStairs = function (n, memo = {}) {
-  if (n in memo) return memo[n];
     if (n <= 0) return 0;
     if (n === 1) return 1;
     if (n === 2) return 2;
-
-    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-    return memo[n];
-};
+    let dp = new Array(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}

b7ef12e936c9bfbd78ca2e7b3e5d61762ae49e5c
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-06-08 16:17:17 +0530

    Time: 1 ms (75.49%) | Memory: 53.8 MB (84.79%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.js
@@ -3,14 +3,27 @@
  * @return {number}
  */
 var maxProduct = function (nums) {
-    let max1 = 0, max2 = 0;
-    for (let num of nums) {
-        if (num > max1) {
+    let max1 = Math.max(nums[0], nums[1])
+    let max2 = Math.min(nums[0], nums[1])
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] > max1) {
             max2 = max1;
-            max1 = num;
-        } else if (num > max2) {
-            max2 = num
+            max1 = nums[i];
+        } else if (nums[i] > max2) {
+            max2 = nums[i]
         }
     }
     return (max1 - 1) * (max2 - 1)
-};
+}
+// var maxProduct = function (nums) {
+//     let max1 = 0, max2 = 0;
+//     for (let num of nums) {
+//         if (num > max1) {
+//             max2 = max1;
+//             max1 = num;
+//         } else if (num > max2) {
+//             max2 = num
+//         }
+//     }
+//     return (max1 - 1) * (max2 - 1)
+// };

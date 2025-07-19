c339582af29c9ba999dd9df86984fc89164eb563
Mahin Pranave <97247845+Mahi0211@users.noreply.github.com>
2025-07-19 23:46:56 +0530

    Time: 1173 ms (5.04%) | Memory: 65.6 MB (94.66%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.js
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -3,6 +3,23 @@
  * @param {number} k
  * @return {boolean}
  */
+
+var containsNearbyDuplicate = function (nums, k) {
+    const seen = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i]
+        if (see.has(num)) {
+            const prevInd = seen.get(num);
+            if (i - prevInd <= k) return true;
+        }
+        seen.set(num, i)
+    }
+    return false;
+}
+
+// brute force
+
 var containsNearbyDuplicate = function (nums, k) {
     let left = 0, right = 1;
     while (left < nums.length) {
